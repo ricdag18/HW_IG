2e00a3616735904c73eed072847ddb4cd3455c05
ricdag18 <riccardo.daguanno@studentmail.unicas.it>
2025-03-26 11:22:31 +0100

    Project

--- project1.js
+++ project1.js
@@ -2,6 +2,13 @@
 // fgImg is the foreground image.
 // fgOpac is the opacity of the foreground image.
 // fgPos is the position of the foreground image in pixels. It can be negative and (0,0) means the top-left pixels of the foreground and background are aligned.
-function composite( bgImg, fgImg, fgOpac, fgPos )
-{
+function composite(bgImg, fgImg, fgOpac, fgPos) {
+
+
+
+
+    //andiamo per prima cosa ad analizzare cosa fa la funzione composite: 
+    // essa permette di andare ad unire due immagini, ovvero un background e un foreground
+
 }
+
